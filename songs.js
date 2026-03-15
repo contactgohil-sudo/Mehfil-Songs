327404220a8a1264f641135a3b235f67b14b7d98
contactgohil-sudo <contact.gohil@gmail.com>
2026-03-15 11:25:57 +0100

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,26 @@
+const songs = [
+  {
+    title: "Tum Hi Ho",
+    movie: "Aashiqui 2",
+    singer: "Arijit Singh",
+    lyrics: "Hum tere bin ab reh nahi sakte tere bina kya wajood mera"
+  },
+  {
+    title: "Kabira",
+    movie: "Yeh Jawaani Hai Deewani",
+    singer: "Arijit Singh Harshdeep Kaur",
+    lyrics: "Kaisi teri khudgarzi na dhoop chune na chhaaon"
+  },
+  {
+    title: "Tujh Mein Rab Dikhta Hai",
+    movie: "Rab Ne Bana Di Jodi",
+    singer: "Roop Kumar Rathod",
+    lyrics: "Tujh mein rab dikhta hai yaara main kya karoon"
+  },
+  {
+  title: "Kal Ho Naa Ho",
+  movie: "Kal Ho Naa Ho",
+  singer: "Sonu Nigam",
+  lyrics: "Har ghadi badal rahi hai roop zindagi"
+  }
+];
